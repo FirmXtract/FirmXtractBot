f39b93b0cc7d1d2034752407af1c2de947b2826f
IMYdev <omranmansour096@gmail.com>
2024-02-24 15:31:11 +0200

    done I guess

--- botcommands.py
+++ botcommands.py
@@ -6,12 +6,18 @@ botid = 7004812988
 
 def command(m): 
     if m.text == "/start":
+        bot.reply_to(m, "Hi, if you want to use me please join here: https://t.me/+_uajqfCeH6Y4ZWJl")
+    if m.text =="/request":
         request(m)
 
 
 def request(m):
-    result = os.system('bash dump.sh')
-    if result == 0:
-        bot.reply_to(m, "Succesfully requested the dump!")
-    else:
-        bot.reply_to(m, "Something went wrong")
+    try:
+        URL = m.text.split()[1]
+        result = os.system(f'bash dump.sh {URL}')
+        if result == 0:
+            bot.reply_to(m, "Succesfully requested the dump!")
+        else:
+            bot.reply_to(m, "Something went wrong")
+    except:
+        bot.reply_to(m, "I need a url to work")
