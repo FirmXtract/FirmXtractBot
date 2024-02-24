00f9ae39af1decb74b8c40639c71ab9a9b1a06bd
IMYdev <omranmansour096@gmail.com>
2024-02-24 15:04:15 +0200

    f around and find out

--- botcommands.py
+++ botcommands.py
@@ -4,7 +4,7 @@ import subprocess
 
 botid = 7004812988
 
-command = """# Check if gh is installed
+command = """
 if ! command -v gh &> /dev/null
 then
     echo "gh could not be found. Installing gh..."
@@ -13,21 +13,16 @@ then
     echo "gh installed."
 fi
 
-# Define the Personal Access Token (PAT)
 PAT="replace with pat"
 
-# Setup gh with the PAT
 gh auth login --with-token $PAT
 
-# Set the repository
 git clone https://github.com/sounddrill31/AndroidDumpsCI.git
 cd AndroidDumpsCI
 gh repo set-default https://github.com/sounddrill31/AndroidDumpsCI.git
 
-# Take URL as an argument
 URL=$1
 
-# Run the GitHub Actions workflow with the specified URL
 gh workflow run DumprX-crave.yml -f ROM_URL=$URL"""
 
 def command(m): 
