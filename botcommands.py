36d53bd5972583931250f5557f55b9701f3d539f
IMYdev <omranmansour096@gmail.com>
2024-02-24 17:09:31 +0200

    HOW, JUST HOW I was so blind to this small bug

--- botcommands.py
+++ botcommands.py
@@ -8,19 +8,19 @@ request_id = -1001263694109
 def command(m): 
     if m.text == "/start":
         bot.reply_to(m, "Hi, if you want to use me please join here: https://t.me/+_uajqfCeH6Y4ZWJl")
-    if m.text =="/request":
+    if m.text.split()[0] =="/request":
         if m.chat.id == request_id:
             request(m)
         else:
             bot.reply_to(m, "Please join this group and use me there: https://t.me/+_uajqfCeH6Y4ZWJl")
 
 def request(m):
-    # try:
-    #     URL = m.text.split()[1]
-    os.system('bash dump.sh')
-    #     if result == 0:
-    #         bot.reply_to(m, "Succesfully requested the dump!")
-    #     else:
-    #         bot.reply_to(m, "Something went wrong")
-    # except:
-    #     bot.send_message(m.chat.id, "I need a url to work")
+    try:
+        URL = m.text.split()[1]
+        result = os.system(f'bash dump.sh {URL}')
+        if result == 0:
+            bot.reply_to(m, "Succesfully requested the dump!")
+        else:
+            bot.reply_to(m, "Something went wrong")
+    except:
+        bot.send_message(m.chat.id, "I need a url to work")
