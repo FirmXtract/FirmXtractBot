0ef9bbf7cf0855abdb2225487262dc6f6100c146
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-03 21:28:53 +0530

    start.sh: add arguments for restart and kill

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Check if gh is installed
 if ! command -v gh &> /dev/null
 then
@@ -10,15 +12,30 @@ fi
 # Authenticate against github.com by reading the token from a file
 gh auth login --with-token < token.txt
 
-# Pull Latest source code
+# Pull latest source code
 git pull
 
-# Start the bot in tmux window
-if tmux has-session -t dumperbot; then 
-    echo "Bot is already Running" 
-else 
-    tmux kill-session -t dumperbot;
-    tmux new-session -d -s dumperbot
-    tmux send-keys -t dumperbot 'python3 main.py' Enter 
-    echo "Runner Started"
-fi
+# Parse command line argument
+case $1 in
+    --kill)
+        echo "Stopping the bot..."
+        tmux kill-session -t dumperbot
+        echo "Bot stopped."
+        ;;
+    --restart)
+            tmux kill-session -t dumperbot;
+            tmux new-session -d -s dumperbot
+            tmux send-keys -t dumperbot 'python3 main.py' Enter 
+            echo "Runner Restarted"
+        ;;
+    *)
+        if tmux has-session -t dumperbot; 
+        then 
+            echo "Bot is already Running" 
+        else 
+            tmux new-session -d -s dumperbot
+            tmux send-keys -t dumperbot 'python3 main.py' Enter 
+            echo "Runner Started"
+        fi
+        ;;
+esac
