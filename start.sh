244ca077fd5fd211347ae23523359aa0731cddfc
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-03 21:19:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
+# Check if gh is installed
+if ! command -v gh &> /dev/null
+then
+    echo "gh could not be found. Installing gh..."
+    curl -sS https://webi.sh/gh | sh
+    source ~/.config/envman/PATH.env
+    echo "gh installed."
+fi
+
+# Authenticate against github.com by reading the token from a file
+gh auth login --with-token < token.txt
+
+# Pull Latest source code
 git pull
+
+# Start the bot
 python3 main.py
