7ec99e29aa24aada9409dfe29dd10d65836be212
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-11 15:48:19 +0530

    dump_gh_actions.sh: add extra command

--- dump_gh_actions.sh
+++ dump_gh_actions.sh
@@ -20,4 +20,4 @@ gh repo set-default https://github.com/OkBuddyGSI/AndroidDumpsCI.git
 URL=$1
 
 # Run the GitHub Actions workflow with the specified URL
-gh workflow run DumprX.yml -f ROM_URL=$URL
+gh workflow run DumprX.yml -f ROM_URL=$URL ROM_URL=$URL EXTRA_CMD="sudo wget https://raw.githubusercontent.com/Fornax96/pdup/master/pdup -O "/usr/local/bin/pdup"; sudo chmod +x "/usr/local/bin/pdup"; pdup out/vbmeta.img"
