b31f312f7ee4e78376203259ae9f0b5d54d1a4cb
IMYdev <omranmansour096@gmail.com>
2024-03-16 23:56:10 +0200

    running some tests

--- botcommands.py
+++ botcommands.py
@@ -75,7 +75,7 @@ def return_workflows(m, workflow):
         list = "cd AndroidDumpsCI && gh run list --workflow=DumprX.yml"
         result = os.popen(list)
         workflows = 1
-        i = result.read().split("Z")
+        i = result.read()
         print(i)
         bot.reply_to(m, i)
         #while workflows < 6:
