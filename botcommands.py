b49a88c00e90bef1cfc7b3304ad7ea231207c71a
IMYdev <omranmansour096@gmail.com>
2024-05-26 12:44:55 +0200

    Add crave dumping and clean up

--- botcommands.py
+++ botcommands.py
@@ -1,11 +1,9 @@
 from info import *
 import validators
-import random
 import os
 
-dump_methods = ["dump_gh_actions.sh"]
+dump_methods = ["dump_gh_actions.sh", "dump_crave.sh"]
 #vndr_gen_script = "vendor_tree.sh"
-botid = 7004812988
 request_id = [-1001263694109, -1002108265780]
 
 def command(m): 
@@ -14,28 +12,31 @@ def command(m):
         bot.send_message(m.chat.id, f"This bot is made by {bot_creator}")
     if m.text.split()[0] =="/request" or m.text.split()[0] =="/dump":
         if m.chat.id in request_id:
-            dump(m)
+            if m.text.split()[2] == "--crave":
+                crave_dump(m)
+            else:
+                dump(m)
         else:
             bot.reply_to(m, "Please join this group and use me there: https://t.me/+_uajqfCeH6Y4ZWJl")
-    if m.text.split()[0] == "/vt":
-        if m.chat.id in request_id:
-            vndr_gen(m)
-        else: 
-            bot.reply_to(m, "Please join this group and use me there: https://t.me/+_uajqfCeH6Y4ZWJl")
-    if m.text.split()[0] == "/workflows":
-        try:
-            request = m.text.split()[1]
-            if request == "vendor":
-                return_workflows(m, "vendor")
-            else:
-                return_workflows(m, "dump")
-        except:
-            bot.reply_to(m, "Something bad happened bruv")
+    # if m.text.split()[0] == "/vt":
+    #     if m.chat.id in request_id:
+    #         vndr_gen(m)
+    #     else: 
+    #         bot.reply_to(m, "Please join this group and use me there: https://t.me/+_uajqfCeH6Y4ZWJl")
+    # if m.text.split()[0] == "/workflows":
+    #     try:
+    #         request = m.text.split()[1]
+    #         if request == "vendor":
+    #             return_workflows(m, "vendor")
+    #         else:
+    #             return_workflows(m, "dump")
+    #     except:
+    #         bot.reply_to(m, "Something bad happened bruv")
 def dump(m):
     try:
         URL = m.text.split()[1]
         validated = validators.url(URL)
-        dump_method = random.choice(dump_methods)
+        dump_method = dump_methods[0]
         url_list = open('bad.txt', 'r')
         lines = url_list.readlines()
         username = bot.get_chat(m.from_user.id).username
@@ -50,7 +51,35 @@ def dump(m):
             else:
                 result = os.system(f'bash {dump_method} {URL}')
                 if result == 0:
-                    bot.reply_to(m, f"Succesfully requested your dump @{username}. Dump URL: {URL}")
+                    bot.reply_to(m, f"Succesfully requested your dump @{username}.\n Dump URL: {URL}")
+                    bot.reply_to(m, "Check @okbuddygsi_stuff for successful dumps")
+                else:
+                    bot.reply_to(m, "Something went wrong")        
+        else:
+            bot.reply_to(m, "Provide me with a valid URL or go play elsewhere")
+    except:
+        bot.send_message(m.chat.id, "Provide me with a valid URL or go play elsewhere")
+
+def crave_dump(m):
+    try:
+        URL = m.text.split()[1]
+        validated = validators.url(URL)
+        dump_method = dump_methods[1]
+        url_list = open('bad.txt', 'r')
+        lines = url_list.readlines()
+        username = bot.get_chat(m.from_user.id).username
+        if validated:
+            URL_check = URL.split("//")[1]
+            URL_check1 = f"{URL_check}\n"
+            URL_check2 = URL_check1.split(".")[1]
+            URL_check3 = f"{URL_check2}\n"
+
+            if URL_check1 in lines or URL_check3 in lines:
+                bot.reply_to(m, "Go fuck yourself you horny bastard")
+            else:
+                result = os.system(f'bash {dump_method} {URL}')
+                if result == 0:
+                    bot.reply_to(m, f"Succesfully requested your dump @{username}.\n Dump URL: {URL}")
                     bot.reply_to(m, "Check @okbuddygsi_stuff for successful dumps")
                 else:
                     bot.reply_to(m, "Something went wrong")        
