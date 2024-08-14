c9bd9e39fc74be74dd1a9069bd0c9f19183d31b0
IMYdev <omranmansour096@gmail.com>
2024-08-14 17:49:57 +0200

    testo x3

--- dump_gh_actions.sh
+++ dump_gh_actions.sh
@@ -17,8 +17,8 @@ cd AndroidDumpsCI
 gh repo set-default https://github.com/OkBuddyGSI/AndroidDumpsCI.git
 
 # Take URL as an argument
-URL=$1
-USERNAME=$2
+URL=$2
+USERNAME=$1
 echo $USERNAME
 # Run the GitHub Actions workflow with the specified URL
-gh workflow run DumprX.yml -f ROM_URL=$URL USERNAME=$USERNAME EXTRA_CMD="sudo wget https://raw.githubusercontent.com/Fornax96/pdup/master/pdup -O "/usr/local/bin/pdup"; sudo chmod +x "/usr/local/bin/pdup"; pdup out/vbmeta.img"
+gh workflow run DumprX.yml -f USER_NAME=$USERNAME ROM_URL=$URL EXTRA_CMD="sudo wget https://raw.githubusercontent.com/Fornax96/pdup/master/pdup -O "/usr/local/bin/pdup"; sudo chmod +x "/usr/local/bin/pdup"; pdup out/vbmeta.img"
